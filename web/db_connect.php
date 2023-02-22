36e22f24f9cfe028994c993f710c5937418ec120
Brian Ellis <brianspencerellis@gmail.com>
2023-02-22 12:09:34 -0700

    db migration

--- web/db_connect.php
+++ web/db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "test";
+$username = "msvgktcdpssoty2c";
+$user_pass = "o5jbhjz73i097fx7";
+$database_in_use = "y21yk2ccq6cwuwc5";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
